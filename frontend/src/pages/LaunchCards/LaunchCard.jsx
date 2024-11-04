4f3889ae14cd44b9051f7162457c8c5ae4dc3cc1
Mitchell Johnson <91237766+Mitchell-jpg@users.noreply.github.com>
2024-11-03 22:56:09 -0500

    UPDATE: Seperated API calls to one folder

--- frontend/src/pages/LaunchCards/LaunchCard.jsx
+++ frontend/src/pages/LaunchCards/LaunchCard.jsx
@@ -1,28 +1,15 @@
 import {useState, useEffect} from "react";
+import {Link} from "react-router-dom";
 import Countdown from "./Countdown.jsx";
+import getLaunchData from "../../api/lldev_calls.js";
 
 function LaunchCards() {
-  const lldevAPI = "https://lldev.thespacedevs.com/2.3.0/launches/upcoming/";
   const [launchData, setLaunchData] = useState([]);
-
-  const getLaunchData = async () => {
-    try {
-      const call = await fetch(lldevAPI);
-      if (!call.ok) {
-        throw new Error("Error");
-      }
-
-      const result = await call.json();
-      return result.results;
-    } catch (error) {
-      console.error(`Error: ${error}`);
-    }
-  };
 
   // Launch cards on initial mount.
   useEffect(() => {
     const fetchLaunchData = async () => {
-      const data = await getLaunchData();
+      const data = await getLaunchData("upcoming");
       setLaunchData(data);
     };
     fetchLaunchData();
@@ -31,21 +18,23 @@ function LaunchCards() {
   return (
     <div className="card-container">
       {launchData.map((launch) => (
-        <div key={launch.id} className="card">
+        <Link to={`/launch/${launch.id}`} key={launch.id} className="card">
           <img
             src={launch.image.thumbnail_url}
             alt={launch.name}
             className="card-image"
           ></img>
-          <h2 className="card-title card-text">{launch.rocket.configuration.full_name}</h2>
+          <h2 className="card-title card-text">
+            {launch.rocket.configuration.full_name}
+          </h2>
           <p className="card-company card-text">{launch.launch_service_provider.name}</p>
           <div className="card-info-container card-text">
             <Countdown net={launch.net} />
           </div>
           <p className="card-location">
-            <a href={launch.pad.wiki_url}>{launch.pad.location.name}</a>
+            <Link to={launch.pad.wiki_url}>{launch.pad.location.name}</Link>
           </p>
-        </div>
+        </Link>
       ))}
     </div>
   );
