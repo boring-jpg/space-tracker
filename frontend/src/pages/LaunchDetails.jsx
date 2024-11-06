b7ec265c921761639f992af52253eba2457ff7ee
Mitchell Johnson <91237766+Mitchell-jpg@users.noreply.github.com>
2024-11-05 23:16:45 -0500

    ADD: Populated details page with information

--- frontend/src/pages/LaunchDetails.jsx
+++ frontend/src/pages/LaunchDetails.jsx
@@ -26,74 +26,88 @@ function LaunchDetails() {
   if (isLoading) return <p>Loading...</p>;
 
   return (
-    <div className="launch-detail-container">
-      <section className="lauch-info">
+    <main className="launch-detail-container">
+      <section className="lauch-detail-info">
         {launchDetail?.image?.thumbnail_url ? (
           <img
             src={launchDetail.image.thumbnail_url}
             alt={launchDetail.image.name || "Launch Image"}
+            className="launch-detail-image"
           />
         ) : (
           <p>No image available</p>
         )}
-        <h1>{launchDetail?.name.split("|")[0]}</h1>
-        <p>
-          Date: <Countdown net={launchDetail.net} />
-        </p>
+        <div className="launch-detail-info-text">
+          <h1>{launchDetail?.name.split("|")[0]}</h1>
+          <Countdown net={launchDetail.net} />
+          <p>
+            <a href={launchDetail.pad.wiki_url}>{launchDetail.pad.name}</a>
+          </p>
+          <p>{launchDetail.pad.location.name}</p>
+        </div>
+        <div className="launch-detail-info-location">
+          <a href={launchDetail.pad.map_url} target="_blank">
+            <img
+              src={launchDetail.pad.map_image}
+              alt={"map of" + launchDetail.pad.location.name}
+              className="launch-details-map-image"
+            ></img>
+          </a>
+        </div>
       </section>
-      <section className="launch-mission">
+
+      {launchDetail.vid_urls
+        .filter((url) => url.url.includes("youtube.com"))
+        .slice(0, 1)
+        .map((url) => (
+          <section className="launch-detail-video" key={url.url}>
+            <iframe
+              width="560"
+              height="315"
+              src={`https://www.youtube.com/embed/${url.url.split("v=")[1]}`}
+              title={url.title}
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </section>
+        ))}
+
+      <section className="launch-detail-mission">
         <h2>Mission: {launchDetail.mission.name}</h2>
         <p>{launchDetail.mission.description || "No mission info provided"}</p>
         {launchDetail.mission.agencies.map((agency, index) => (
-          <div className="launch-agency" key={index}>
-            <img
-              src={agency.logo?.image_url || "No logo image provided"}
-              alt={agency.logo?.name || "logo image"}
-            ></img>
-            <p>
-              <a href={agency.url}>Name: {agency.name}</a>
-            </p>
-            <p>{agency.description}</p>
-            <p></p>
+          <div className="launch-detail-agency" key={index}>
+            <a href={agency.wiki_url}>
+              <img
+                src={agency.logo?.image_url || "No logo image provided"}
+                alt={agency.logo?.name || "logo image"}
+                className="launch-detail-agency-logo"
+              ></img>
+            </a>
           </div>
         ))}
       </section>
 
-      <section className="launch-video">
-        {/* Display's firsy available youtube video to ensure an embedded video */}
-        {launchDetail.vid_urls
-          .filter((url) => url.url.includes("youtube.com"))
-          .slice(0, 1)
-          .map((url) => (
-            <div key={url.url}>
-              <h3>{url.title}</h3>
-              <p>{url.description}</p>
-              <iframe
-                width="560"
-                height="315"
-                src={`https://www.youtube.com/embed/${url.url.split("v=")[1]}`}
-                title={url.title}
-                frameBorder="0"
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </div>
+      <section className="launch-detail-updates">
+        <div className="launch-detail-update-container">
+          {launchDetail.updates.map((update) => (
+            <article className="launch-detail-update" key={update.id}>
+              <img
+                className="launch-detail-update-image"
+                src={update.profile_image}
+                alt={"Photo of" + update.created_by}
+              ></img>
+              <div className="launch-detail-update-text">
+                <p>{update.comment}</p>
+                <p>
+                  {update.created_by} | {update.created_on}
+                </p>
+              </div>
+            </article>
           ))}
-
-        {/* Displays Official Video if available */}
-        {launchDetail.vid_urls
-          .filter((url) => url.type.name.startsWith("Official"))
-          .map((url) => (
-            <div key={url.url}>
-              <h3>{url.title}</h3>
-              <p>{url.description}</p>
-              <a href={url.url} target="_blank" rel="noopener noreferrer">
-                Watch Video
-              </a>
-            </div>
-          ))}
+        </div>
       </section>
-    </div>
+    </main>
   );
 }
 
