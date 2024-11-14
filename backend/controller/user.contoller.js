0854f6818c9cd80b904a9e6372bdd00d3d6d33be
Mitchell Johnson <91237766+Mitchell-jpg@users.noreply.github.com>
2024-11-14 00:04:37 -0500

    ADD: ability to create new users

--- backend/controller/user.contoller.js
+++ backend/controller/user.contoller.js
@@ -3,12 +3,32 @@ import User from "../models/user.model.js"
 export const loginUser = async (req, res) => {
     res.status(200).json({
         msg: 'login user'
-    })
-}
+    });
+};
 
 export const signupUser = async (req, res) => {
-    res.status(200).json({
-        msg: 'signup user'
-    })
-}
+    const {email, password} = req.body
+
+    console.log('\ncreating user...')
+    
+    try{
+
+        const user = await User.signup(email, password);
+
+        res.status(200).json({
+            success: true,
+            user
+        });
+
+        console.log(`\nUser ${email} created`)
+
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+            error: err.message
+        });
+
+        console.log(`\nrejected ${email}. Email aleardy in use.`)
+    };
+};
 
