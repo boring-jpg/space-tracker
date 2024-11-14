e6c82c6b40265ec57e316d84087c34b062e69cee
Mitchell Johnson <91237766+Mitchell-jpg@users.noreply.github.com>
2024-11-14 00:03:00 -0500

    UPDATE: Allow express to pase json input from req

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,25 @@ import userRoutes from './routes/user.route.js'
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT;
 
+// allow express to parse body
+app.use(express.json());
+
+// endpoints
 app.use('/api/user/', userRoutes)
 
 
-app.listen(3000, async () => {
-    await connectDB();
-    console.log('listning on port 3000');
+app.listen(port, async () => {
+    
+    try{
+
+        console.log("Establishing conection to MongoDB...");
+        await connectDB();
+
+    } catch (err) {
+        console.error(err.message);
+    }
+
+    console.log(`\nServer successfully started. Listning on http://localhost:${port}`);
 });
